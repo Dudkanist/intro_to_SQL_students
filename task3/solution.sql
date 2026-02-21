3b59f08e80bba304e1415c108997dbf8b329ae21
Vladislav Mantsevich <vladmanest@gmail.com>
2026-02-21 09:39:44 +0300

    solution2

--- task3/solution.sql
+++ task3/solution.sql
@@ -1,18 +1,25 @@
+ALTER TABLE server_logs ADD COLUMN Session_Dur REAL;
+
+UPDATE server_logs
+SET Session_Dur = CASE
+WHEN Session_Start IS NOT NULL AND Session_End IS NOT NULL
+THEN (julianday(Session_End) - julianday(Session_Start)) * 24.0 * 60.0
+ELSE NULL
+END;
+
 DROP VIEW IF EXISTS v_users_activity;
+
 CREATE VIEW v_users_activity AS
 SELECT
 u.User_ID,
 u.First_Name,
 u.Last_Name,
-COUNT(l.Log_ID) AS Num_Sessions,
-COALESCE(SUM(
-CASE
-WHEN l.Session_Start IS NOT NULL AND l.Session_End IS NOT NULL
-THEN (julianday(l.Session_End) - julianday(l.Session_Start)) * 24.0 * 60.0
-ELSE 0
-END
-), 0) AS Total_Session_Time
+COUNT(s.Log_ID) AS Num_Sessions,
+COALESCE(SUM(s.Session_Dur), 0) AS Total_Session_Time
 FROM users u
-LEFT JOIN logs l ON u.User_ID = l.User_ID
-GROUP BY u.User_ID, u.First_Name, u.Last_Name
-ORDER BY Total_Session_Time DESC;
+LEFT JOIN server_logs s
+ON u.User_ID = s.User_ID
+GROUP BY
+u.User_ID, u.First_Name, u.Last_Name
+ORDER BY
+Total_Session_Time DESC;
